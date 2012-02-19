c7e2bcbb55d6abaa8f8b1f18d4d9ee130192cd16
Usein Mambediiev <useinm@gmail.com>
2012-02-19 19:22:45 +0200

    Initial commit

---
+++ Scripts/unit_tests.js
@@ -0,0 +1,138 @@
+﻿$(document).ready(function() {
+    module("unit tests");
+
+    var baseAddress = "http://localhost/ODataService/";
+
+    asyncTest("testing 'from' method", function() {
+        ODataLinq
+            .from(baseAddress + "Products")
+            .select(function(data) {
+                ok(data != null && data.length > 0);
+                start();
+            });
+    });
+
+    asyncTest("testing 'orderby' method", function() {
+        var checkSorting = function(data, field, isAsc) {
+            var array = $.map(data, function(product) {
+                return product[field];
+            });
+            array.sort();
+            if (!isAsc) {
+                array.reverse();
+            }
+            var isSorted = true;
+            $.each(array, function(key, value) {
+                isSorted = data[key][field] == value;
+            });
+            return isSorted;
+        };
+
+        ODataLinq
+            .from(baseAddress + "Products")
+            .orderby("Name")
+            .select(function(data) {
+                ok(checkSorting(data, "Name", true), "Single field");
+                start();
+            });
+        
+        ODataLinq
+            .from(baseAddress + "Products")
+            .orderby("-Name")
+            .select(function(data) {
+                ok(checkSorting(data, "Name", false), "Single field, descending order");
+                start();
+            });
+    });
+
+    asyncTest("testing 'take' method", function() {
+        ODataLinq
+            .from(baseAddress + "Products")
+            .take(3)
+            .select(function(data) {
+                ok(data != null && data.length == 3);
+                start();
+            });
+    });
+
+    asyncTest("testing 'skip' method", function() {
+        ODataLinq
+            .from(baseAddress + "Products")
+            .skip(5)
+            .select(function(data) {
+                ok(data != null);
+                start();
+            });
+    });
+
+    asyncTest("testing filtering methods", function() {
+        ODataLinq
+            .from(baseAddress + "Products")
+            .equals("Name", "Bread")
+            .or()
+            .not().equals("Name", "Eggs")
+            .select(function(data) {
+                ok(data != null && $.grep(data,
+                        function(product) {
+                            return product.Name == "Bread" || product.Name != "Eggs";
+                        }).length == data.length,
+                    "'equals', 'or', 'not'");
+                start();
+            });
+
+        ODataLinq
+            .from(baseAddress + "Products")
+            .notEquals("Name", "Bread")
+            .select(function(data) {
+                ok(data != null && $.grep(data,
+                    function(product) {
+                        return product.Name != "Bread";
+                    }).length == data.length, "'notEquals'");
+                start();
+            });
+
+        ODataLinq
+            .from(baseAddress + "Products")
+            .greater("Id", 5)
+            .select(function(data) {
+                ok(data != null && $.grep(data,
+                    function(product) {
+                        return product.Id > 5;
+                    }).length == data.length, "'greater'");
+                start();
+            });
+
+        ODataLinq
+            .from(baseAddress + "Products")
+            .greaterEquals("Id", 6)
+            .select(function(data) {
+                ok(data != null && $.grep(data,
+                    function(product) {
+                        return product.Id >= 6;
+                    }).length == data.length, "'greaterEquals'");
+                start();
+            });
+
+        ODataLinq
+            .from(baseAddress + "Products")
+            .less("Id", 3)
+            .select(function(data) {
+                ok(data != null && $.grep(data,
+                    function(product) {
+                        return product.Id < 3;
+                    }).length == data.length, "'less'");
+                start();
+            });
+
+        ODataLinq
+            .from(baseAddress + "Products")
+            .lessEquals("Id", 2)
+            .select(function(data) {
+                ok(data != null && $.grep(data,
+                    function(product) {
+                        return product.Id <= 2;
+                    }).length == data.length, "'lessEquals'");
+                start();
+            });
+    });
+});
---
+++ Scripts/xhr_stub.js
@@ -0,0 +1,44 @@
+﻿(function(window, undefined) {
+    var stubData = {
+        'http://localhost/ODataService/Products':
+            '{"d": [{"Id": 1, "Name": "Meat"}, {"Id": 2, "Name": "Bread"}]}',
+        'http://localhost/ODataService/Products?$orderby=Name':
+            '{"d": [{"Id": 2, "Name": "Bread"}, {"Id": 1, "Name": "Meat"}]}',
+        'http://localhost/ODataService/Products?$orderby=Name desc':
+            '{"d": [{"Id": 1, "Name": "Meat"}, {"Id": 2, "Name": "Bread"}]}',
+        'http://localhost/ODataService/Products?$top=3':
+            '{"d": [{"Id": 1, "Name": "Meat"}, {"Id": 2, "Name": "Bread"}, {"Id": 3, "Name": "Eggs"}]}',
+        'http://localhost/ODataService/Products?$skip=5':
+            '{"d": [{"Id": 6, "Name": "Olive oil"}, {"Id": 7, "Name": "Butter"}]}',
+        "http://localhost/ODataService/Products?$filter=Name eq 'Bread' or not (Name eq 'Eggs')":
+            '{"d": [{"Id": 1, "Name": "Meat"}, {"Id": 2, "Name": "Bread"}]}',
+        "http://localhost/ODataService/Products?$filter=Name ne 'Bread'":
+            '{"d": [{"Id": 6, "Name": "Olive oil"}, {"Id": 7, "Name": "Butter"}]}',
+        'http://localhost/ODataService/Products?$filter=Id gt 5':
+            '{"d": [{"Id": 6, "Name": "Olive oil"}, {"Id": 7, "Name": "Butter"}]}',
+        'http://localhost/ODataService/Products?$filter=Id ge 6':
+            '{"d": [{"Id": 6, "Name": "Olive oil"}, {"Id": 7, "Name": "Butter"}]}',
+        'http://localhost/ODataService/Products?$filter=Id lt 3':
+            '{"d": [{"Id": 1, "Name": "Meat"}, {"Id": 2, "Name": "Bread"}]}',
+        'http://localhost/ODataService/Products?$filter=Id le 2':
+            '{"d": [{"Id": 1, "Name": "Meat"}, {"Id": 2, "Name": "Bread"}]}'
+    };
+
+    window.XMLHttpRequest = function() {
+        this.open = function(method, url, isAsync) {
+            this.url = url;
+        };
+        this.setRequestHeader = function(key, value) {
+        };
+        this.send = function(data) {
+            this.readyState = 4;
+            this.status = 200;
+            this.responseText = stubData[this.url];
+            if (this.responseText === undefined) {
+                throw new Error("Not existing key: " + this.url);
+            }
+            var that = this;
+            setTimeout(function() { that.onreadystatechange(); }, 1);
+        };
+    };
+})(window);
